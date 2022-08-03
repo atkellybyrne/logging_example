16aad957525161050fd51d1d2e3b6dfd497bd442
Kelly Byrne <95244047+atkellybyrne@users.noreply.github.com>
2022-08-03 15:28:05 -0400

    Add files via upload

--- sample_logger.py
+++ sample_logger.py
@@ -1,29 +1,6 @@
 # Imports the logging library
 import logging
-
-#
-# ******************************************************
-#                       VARIABLES
-# ******************************************************
-#
-
-# Creates a logging object and gets the name of the root
-logger = logging.getLogger(name = __name__)
-
-# Sets the default level of the logger to DEBUG
-logger.setLevel(logging.DEBUG)
-
-# Creates a Formatter object that sets the format of the log message
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-
-# Creates a FileHandler object that sets the .log file
-file_handler = logging.FileHandler('sample_logger.log')
-
-# Sets the format for the FileHandler to the Formatter object
-file_handler.setFormatter(formatter)
-
-# Adds the handler from the FileHandler object to the logging object
-logger.addHandler(file_handler)
+import log_class
 
 #
 # ******************************************************
@@ -37,7 +14,7 @@ logger.addHandler(file_handler)
 
 def add(x, y):
     """Add Function"""
-    return logger.info(f'Add: {num_1} + {num_2} = {x+y}')
+    return log_class.log('+', x, y, x+y)
 
 # A subtract method that takes in two varaibles, x and y and
 # subtracts x from y
@@ -45,7 +22,7 @@ def add(x, y):
 
 def subtract(x, y):
     """Subtract Function"""
-    return logger.info(f'Sub: {num_1} - {num_2} = {x-y}')
+    return log_class.log('-', x, y, x-y)
 
 # A multiply method that takes in two variables, x and y and
 # multiplies x by y
@@ -53,7 +30,7 @@ def subtract(x, y):
 
 def multiply(x, y):
     """Multiply Function"""
-    return logger.info(f'Mul: {num_1} * {num_2} = {x*y}')
+    return log_class.log('*', x, y, x*y)
 
 # A divide method that takes in two variables, x and y and
 # divides x from y
@@ -62,18 +39,12 @@ def multiply(x, y):
 
 def divide(x, y):
     """Divide Function"""
-    return logger.info(f'Div: {num_1} / {num_2} = {x/y}')
-
-# An error method that checks for a ZeroDivisionError
-
-
-def error(x, y):
     try:
-        x / y
+        x / y 
     except ZeroDivisionError:
-        return True
+        return log_class.error()
     else:
-        return False
+        return log_class.log('/', x, y, x/y)
 
 #
 # ******************************************************
@@ -81,21 +52,11 @@ def error(x, y):
 # ******************************************************
 #
 
-
 # Creates a num_1 and num_2 object
 num_1 = 3
-num_2 = 8
+num_2 = 0
 
-# Creates a error(x, y) method object named error_Check
-error_Check = error(num_1, num_2)
-
-# Runs error(x,y) and if it is false, will run the other methods like
-# add(), subtract(), and multiply()
-if(error_Check is False):
-    logger.info("There were no errors present")
-    add(num_1, num_2)
-    subtract(num_1, num_2)
-    multiply(num_1, num_2)
-    divide(num_1, num_2)
-else:
-    logger.warning("Tried to divide by zero")
+add(num_1, num_2)
+subtract(num_1, num_2)
+multiply(num_1, num_2)
+divide(num_1, num_2)
